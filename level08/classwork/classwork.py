647534871b211d7462ac0563f3a0a227c5e31b72
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2025-05-14 22:39:33 +0400

    GOA

---
+++ level08/classwork/classwork.py
@@ -0,0 +1,8 @@
+# მომხმარებელს შემოატანინეთ თავისი სახელი და შეინახეთ ცვლადში, ასევე შემოატანინეთ მისი გვარი და ასაკი და f სტრინგის გამოყენებით დაბეჭდეთ მსგავსი წინადადება:
+
+# "The user's name is სახელი. Her surname is გვარი and she is ასაკი years old.
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = int(input("enter your age: "))
+print(f"The user's name is {name} Her surname is {surname} and she is {age} years old.")
